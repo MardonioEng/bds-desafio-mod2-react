e8b975121e9957c67a5dacd235bdb6867e597af3
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-04-10 10:57:40 -0300

    Componente ProfileShow

--- src/pages/ProfileSearch/index.tsx
+++ src/pages/ProfileSearch/index.tsx
@@ -1,13 +1,19 @@
+import ProfileShow from "components/ProfileShow";
 import "./styles.css";
 
 const ProfileSearch = () => {
-  return (
-    <div className="search-container">
-      <h1>Encontre um perfil Github</h1>
-      <input type="text" placeholder="Usuário Github" />
-      <button>Encontrar</button>
-    </div>
-  )
-}
+	return (
+		<div className="container">
+			<div className="search-container">
+				<h1>Encontre um perfil Github</h1>
+				<input type="text" placeholder="Usuário Github" />
+				<button>Encontrar</button>
+			</div>
+			<div className="infos-container">
+				<ProfileShow />
+			</div>
+		</div>
+	);
+};
 
 export default ProfileSearch;
