6969e2f748f541fd6832975a89f42d9ee74218f6
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-05-04 14:42:32 -0300

    Integração com API Github

--- src/pages/ProfileSearch/index.tsx
+++ src/pages/ProfileSearch/index.tsx
@@ -1,19 +1,63 @@
-import ProfileShow from "components/ProfileShow";
-import "./styles.css";
+import axios from 'axios';
+import ProfileShow from 'components/ProfileShow';
+import { useState } from 'react';
+import './styles.css';
+
+type FormData = {
+	username: string;
+}
+
+type Profile = {
+	avatar_url: string,
+  url: string,
+  name: string,
+  location: string,
+  followers: number,
+}
 
 const ProfileSearch = () => {
+	const [profile, setProfile] = useState<Profile>();
+	const [formData, setFormData] = useState<FormData>({
+    username: '',
+  });
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		setFormData({ ...formData, [name]: value });
+		console.log(formData);
+	}
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		axios.get(`https://api.github.com/users/${formData.username}`)
+			.then(response => setProfile(response.data))
+			.catch((error) => setProfile(undefined));
+	}
+
 	return (
 		<div className="container">
 			<div className="search-container">
 				<h1>Encontre um perfil Github</h1>
-				<input type="text" placeholder="Usuário Github" />
-				<button>Encontrar</button>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						name="username"
+						placeholder="Usuário Github"
+						value={formData.username}
+						onChange={handleChange}
+					/>
+					<button>Encontrar</button>
+				</form>
 			</div>
 			<div className="infos-container">
-				<ProfileShow />
+				{	profile &&
+					<ProfileShow {...profile} />
+				}
 			</div>
 		</div>
 	);
 };
 
 export default ProfileSearch;
+
